0eac494cad027102452d6d4023065aa44ffdf0bc
Yashikab <yashikaba@gmail.com>
2020-06-29 08:19:32 +0900

    test

--- scripts/git-diff-make.sh
+++ scripts/git-diff-make.sh
@@ -13,6 +13,9 @@ for TARGET in $TARGETS
 do
     HAS_DIFF=`git diff --diff-filter=ACMR --name-only HEAD\^ HEAD --relative=$TARGET | head -1`
     if [ ! -z $HAS_DIFF ]; then
+        echo "Pushing on" ${TARGET} "to docker"
         ( cd ${TARGET} && make $1 )
+    else
+        echo "No changes has founded"
     fi
 done
