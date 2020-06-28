18d114da904d17ba4b0389b894a4f605910ae989
Yashikab <yashikaba@gmail.com>
2020-06-28 11:43:47 +0900

    test

--- scripts/git-diff-make.sh
+++ scripts/git-diff-make.sh
@@ -8,7 +8,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-TARGETS=(python)
+TARGETS=(`find types -maxdepth 1 -mindepth 1 -type d`)
 for TARGET in $TARGETS
 do
     HAS_DIFF=`git diff --diff-filter=ACMR --name-only HEAD\^ HEAD --relative=$TARGET | head -1`
